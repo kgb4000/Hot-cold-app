baf579d9935ca3e4f93a83afc698d38812036c72
KGB4000 <kester4000@gmail.com>
2015-11-12 16:54:16 -0500

    Changed js in js filed, built new game button function, added guesscount = 0; in variables at the top of the page

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
 	var $userGuess = $('#userGuess');
 	var $feedback = $('#feedback');
 	var guessCount = 	$('#count').val();
+	guessCount = 0;
+	var secretNumber;
+
+	getSecretNumber();
 	
 	// Display information modal box
   	$(".what").click(function(){
@@ -15,45 +19,38 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
- 	// Start a new game
-  newGame = function() {
-  	$('a.new').on('click', function() {
-  		getSecretNumber();
-  		$userGuess.val('');
-  		$feedback.html('Make your Guess!');
-  	});
-  }
-
-  newGame();
-
-  getSecretNumber();
-
+  
   // Generate random number between 1 and 100
   function getSecretNumber() {
-	  var secretNumber = Math.floor((Math.random() * 100) + 1);
+	  secretNumber = Math.floor((Math.random() * 100) + 1);
 	  console.log(secretNumber);
 
+	  // Update user guess count
+			
 		// Get user's guess
 		$('form').on('submit', function(e) {
 			e.preventDefault();
+			
 
-			// Update user guess count
-			guessCount++;
-			console.log(guessCount);
-		  
-		  // Write user guess count into page
-		  // $('#count').text(guessCount);
-		 
 		 // Get user's guess
 			var userGuess = $('#userGuess').val();
 
 			// User guesses that are not correct, are added to a list
 			userGuesses = function() {
-		  	$('#guessList').append('<li>' + userGuess + '</li>');
+		  	$('#guessList').append('<li class="guesses">' + userGuess + '</li>');
 		  }
 
-			if (userGuess == secretNumber) {
+		  if(userGuess > 100 || userGuess < 1){
+		  	$feedback.html('Enter a number between 1 and 100');
+		  	userGuesses();
+		  	$userGuess.val('');
+		  }
+
+		  else if (isNaN(userGuess) === true) {
+		  	alert('That is not a number');
+		  }
+
+			else if (userGuess == secretNumber) {
 				$feedback.html('That is correct');
 				userGuesses();
 			} 
@@ -83,14 +80,48 @@ $(document).ready(function(){
 				console.log("Not even close!");
 				userGuesses();
 			}
+
 			$userGuess.val('');
 
+			guessCount++;
+
+			console.log(guessCount);
+			console.log('user guess count is: ' + guessCount);
+
 			$('#count').text(guessCount);
+
 
 		});
 
 	}
 
+	// Start new game
+	$('a.new').on('click', function(e) {
+		e.preventDefault();
+		
+		// Set new secret number
+		secretNumber = Math.floor((Math.random() * 100) + 1);
+		console.log(secretNumber);
+
+		// Reset feedback message
+		$feedback.html('Make your Guess!');
+		// 
+		$('#count').val('');
+		$userGuess.val('');
+
+		// Remove user guesses
+		$('li.guesses').remove();
+
+		// Set guess count to 0
+		guessCount = 0;
+		console.log('user guess count is: ' + guessCount);
+
+		// Set user guess count to show 0 in html
+		$('#count').text('0');
+	});
+
 });
+
+
 
 
